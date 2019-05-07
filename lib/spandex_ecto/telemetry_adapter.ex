9ef75cae60d2d5b4a2ab12bc0f139defa1240603
Qing Wu <wiserfirst@gmail.com>
2019-05-07 22:57:51 +1000

    add TelemetryAdapter module and update Readme to support Ecto 3

---
+++ lib/spandex_ecto/telemetry_adapter.ex
@@ -0,0 +1,23 @@
+defmodule SpandexEcto.TelemetryAdapter do
+  alias SpandexEcto.EctoLogger
+
+  #  this is for ecto_sql 3.0.x
+  def handle_event([_app_name, repo_name, :query], total_time, log_entry, _config) when is_integer(total_time) do
+    EctoLogger.trace(log_entry, "#{repo_name}_database")
+  end
+
+  # This is for ecto_sql >= 3.1
+  def handle_event([_app_name, repo_name, :query], measurements, metadata, _config) when is_map(measurements) do
+    log_entry = %{
+      query: metadata.query,
+      source: metadata.source,
+      params: metadata.params,
+      query_time: Map.get(measurements, :query_time, 0),
+      decode_time: Map.get(measurements, :decode_time, 0),
+      queue_time: Map.get(measurements, :queue_time, 0),
+      result: {metadata.result, "n/a"}
+    }
+
+    EctoLogger.trace(log_entry, "#{repo_name}_database")
+  end
+end
